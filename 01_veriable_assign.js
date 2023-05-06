085907992f09406538741af59205b9e861017f70
pramodinidete9696 <pramodinidete9696@gmail.com>
2023-05-06 11:50:28 +0530

    updated js

---
+++ 01_veriable_assign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log(myFullName);
+var myFullName="pramodini dete"
+console.log(myFullName);
+myFullName="pramodini rajesh dete";
+console.log(myFullName);
+
+var myLove="I love only js"
+console.log(myLove);
+
+var age="24";
+console.log(age);
+
+var address="At Rui Tal:-Dharashiv Dist:-Dharashiv";
+console.log(address);
+
+var pincode="413501";
+console.log(pincode);
+
+var gender="Female"
+console.log(gender);
+ 
+var isMarried="no";
+console.log(isMarried);
---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log("pramodini dete....");
+console.log("my friend name is priya");
+console.log(" address:Dharashiv");
+
+ var firstName="pramodini";
+ console.log(firstName);
+ var age; //veriable declaration
+ age="22" //veriable initialization
+ var holder;
+ console.log(holder);//undefined
+ holder="my spec"; //veriable initialization
+ console.log(holder); 
+ holder="sachin spec" //veriable update
+ console.log(holder);
