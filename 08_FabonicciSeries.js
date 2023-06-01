a60bcde87fddb6fbaba0a151e3957d3187a568eb
pramodinidete9696 <pramodinidete9696@gmail.com>
2023-06-01 19:24:03 +0530

    fabonicciSeries
    fabonicciSeries

---
+++ 08_FabonicciSeries.js
@@ -0,0 +1,19 @@
+
+
+// 
+
+
+var n1=0
+var n2=1
+var result = []
+for (let index = 0; index <9; index++) {
+//    console.log(n1);
+   result= result + " " +n1 
+    sum = n2+n1;
+    n1=n2;
+    n2= sum
+  
+}
+console.log(`Fibonacci Series :- [${result}]`);
+      
+    
